d817637e60940345b25f48a8df3c0a18e323cd27
Michael <meister@spacekace.com>
2017-11-03 09:59:55 -0700

    tweak

--- app/models/data_point.rb
+++ app/models/data_point.rb
@@ -27,7 +27,7 @@ class DataPoint < ActiveRecord::Base
 	def acceleration_vector_angle_zaxis
 		# to_degrees( Math.asin( acceleration_xaxis / acceleration_vector_length.to_f ) )
 		angle = to_degrees( Math.atan2( acceleration_xaxis, acceleration_yaxis ) )
-		if acceleration_xaxis > 0
+		if acceleration_zaxis > 0
 			angle = angle - 45
 			angle = 360 + angle if angle < 180
 		else
