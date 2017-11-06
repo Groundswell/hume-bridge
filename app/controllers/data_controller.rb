60fecb89d1f2cdd7e9b04d3bfa86c4949eafdab6
Michael <meister@spacekace.com>
2017-11-06 08:17:35 -0800

    no tags

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -27,7 +27,10 @@ class DataController < ActionController::Base
 		# @seconds_per_datapoint = 6
 		# @seconds_per_datapoint = ( 60.0 / DataPoint.where( created_at: last_data_point.created_at - 1.minute..last_data_point.created_at ).count ).round if last_data_point.present?
 
-		@data_points = DataPoint.where( 'id > ?', last_index ).order( id: :asc ).limit(@limit)
+		@data_points = DataPoint.where( 'id > ?', last_index )
+		@data_points = @data_points.with_all_tags( params[:tags] ) if params[:tags].present?
+		@data_points = @data_points.where( tags: '{}' ) if params[:no_tags].present?
+		@data_points = @data_points.order( id: :asc ).limit(@limit)
 
 		render layout: false, status: 200
 
