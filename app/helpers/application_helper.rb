0aac138fd4ceeb77a299bdf325863b62544190f8
Michael <meister@spacekace.com>
2017-11-07 11:02:28 -0800

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,25 +8,25 @@ module ApplicationHelper
 
 		if i < 256
 			r = i
-		elsif i >= 256
+		elsif i < 512
 			r = 255
 			g = (i - 255)
-		elsif i >= 512
+		elsif i < 768
 			r = 255 - (i - 511)
 			g = 255
-		elsif i >= 768
+		elsif i < 1024
 			g = 255
 			b = (i - 768)
-		elsif i >= 1024
+		elsif i < 1280
 			g = 255 - (i - 1023)
 			b = 255
-		elsif i >= 1280
+		elsif i < 1536
 			b = 255
 			r = (i - 1280)
-		elsif i >= 1536
+		elsif i < 1792
 			b = 255 - (i - 1535)
 			r = 255
-		elsif i >= 1792
+		else
 		end
 
 
