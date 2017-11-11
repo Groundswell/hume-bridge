0590280a1d3d3b28c0c94c2507141e1d489ab990
Michael <meister@spacekace.com>
2017-11-10 18:36:24 -0800

    tweak

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -43,8 +43,8 @@ class DataController < ActionController::Base
 		@data_points = @data_points.where( tags: '{}' ) if params[:no_tags].present?
 		@data_points = @data_points.where( device_id: params[:device_id] ) if params[:device_id].present?
 
-
-		last_index = (params[:since] || @data_points.order(id: :desc).offset(@limit).pluck(:id).first || 0).to_i
+		last_index = params[:since] unless params[:since].blank?
+		last_index ||= (@data_points.order(id: :desc).offset(@limit).limit(1).pluck(:id).first || 0).to_i
 
 		@data_points = @data_points.where( 'id > ?', last_index )
 		@data_points = @data_points.order( id: :asc ).limit(@limit)
